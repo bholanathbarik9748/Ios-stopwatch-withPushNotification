8c29e3e5c92ef1e895b63bc49fad617dc58ddbdf
Bholanath <bholanathbarik9748@gmail.com>
2024-09-06 16:55:23 +0530

    Initial Commit

---
+++ tummoc-stopWatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tummoc-stopWatch
+//
+//  Created by Bholanath Barik on 06/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tummoc-stopWatch/tummoc_stopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  tummoc_stopWatchApp.swift
+//  tummoc-stopWatch
+//
+//  Created by Bholanath Barik on 06/09/24.
+//
+
+import SwiftUI
+
+@main
+struct tummoc_stopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
